7634850410f0770c6d4cf9325be97a2ab09af053
chnasko123 <81710612+chnasko123@users.noreply.github.com>
2021-04-07 20:38:48 -0400

    Update authenticate.php

--- login/authenticate.php
+++ login/authenticate.php
@@ -1,29 +1,47 @@
 <?php
-//for this we'll turn on error output so we can try to see any problems on the screen
-//this will be active for any script that includes/requires this one
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-function getDB(){
-    global $db;
-    //this function returns an existing connection or creates a new one if needed
-    //and assigns it to the $db variable
-    if(!isset($db)) {
-        try{
-            //__DIR__ helps get the correct path regardless of where the file is being called from
-            //it gets the absolute path to this file, then we append the relative url (so up a directory and inside lib)
-            require_once(__DIR__. "/../lib/config.php");//pull in our credentials
-            //use the variables from config to populate our connection
-            $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
-            //using the PDO connector create a new connect to the DB
-            //if no error occurs we're connected
-            $db = new PDO($connection_string, $dbuser, $dbpass);
+    require(__DIR__ . "/../lib/myFunctions.php");
+    if(isset($_REQUEST["email"])){
+        $email = $_REQUEST["email"];
+        $password = $_REQUEST["password"];
+        if(is_empty_or_null($email) || is_empty_or_null($password)){
+            echo "Something's missing here....";
+            exit();
+        } 
+        require(__DIR__ . "/../lib/db.php");
+        //$db = getDB();//invoked in db.php already
+        $email = mysqli_real_escape_string($db, $email);
+        $hash = password_hash($password, PASSWORD_BCRYPT);
+        //mysqli still wants the single quotes in the query so can't just drop in the variables post-escape
+        $sql = "SELECT id, email, password from mt_users where email = '$email'";
+        $retVal = mysqli_query($db, $sql);
+        if($retVal){
+            $result = mysqli_fetch_array($retVal, MYSQLI_ASSOC);
+            var_export($result);
+            if(password_verify($password, $result["password"])){
+                echo "<br>";
+                echo "Hey you logged in!";
+                unset($result["password"]);
+                session_start();
+                $_SESSION["user"] = $result;
+                die(header("Location: home.php"));
+                //echo "<br>";
+                //var_export($_SESSION);
+            }
+            else{
+                echo "You're not you, go away";
+            }
         }
-    catch(Exception $e){
-            var_export($e);
-            $db = null;
+        else{
+            echo "Something didn't work out " . mysqli_error($db);
         }
+        //TODO: don't forget to close your connection, don't want resource leaks
+        mysqli_close($db);
     }
-    return $db;
-}
 ?>
+<form method="POST">
+<label>Email</label>
+<input type="text" name="email"/>
+<label>Password</label>
+<input type="password" name="password"/>
+<input type="submit" value="Login"/>
+</form>
