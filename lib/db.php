8e41f837610a7a9f657fd6e9e59cffac2043e2d8
chnasko123 <81710612+chnasko123@users.noreply.github.com>
2021-04-07 20:35:47 -0400

    Update db.php

--- lib/db.php
+++ lib/db.php
@@ -1,22 +1,20 @@
 <?php
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 function getDB(){
-  global $db;  
+  global $db;
   if(!isset($db)){
-    error_reporting(E_ERROR|E_WARNING|E_PARSE|E_NOTICE);
-    ini_set('display_errors',1);
-
-    include (  "accounts.php"     ) ;
-    $db = mysqli_connect($hostname,$username,$password,$project );
-    if (mysqli_connect_errno())
-    {     
-      echo "Failed to connect to MySQL: " . mysqli_connect_error();  
-		  exit();
+    require (__DIR__."/account.php") ; 
+    $db = mysqli_connect($hostname,$username,$password, $project );
+    if (mysqli_connect_errno()){      
+        echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        exit();
     }
     print "You have successfully connected to MySQL database.<br>";
     mysqli_select_db( $db, $project );
   }
   return $db;
 }
-
+$db = getDB();//This is valid
 ?>
