57554ae6d80c9f117ae984c243d1f977c0bd6fe4
chnasko123 <81710612+chnasko123@users.noreply.github.com>
2021-04-07 20:40:22 -0400

    Update register.php

--- login/register.php
+++ login/register.php
@@ -1,63 +1,42 @@
-<p>Run me in the browser from your server to try</p>
-<form method="POST">
-  <label for="email">Email:</label>
-  <input type="email" id="email" name="email" required/>
-  <label for="p1">Password:</label>
-  <input type="password" id="p1" name="password" required/>
-  <label for="p2">Confirm Password:</label>
-  <input type="password" id="p2" name="confirm" required/>
-  <input type="submit" name="register" value="Register"/>
-</form>
 <?php
-if(isset($_POST["register"])){
-  $email = null;
-  $password = null;
-  $confirm = null;
-  if(isset($_POST["email"])){
-    $email = $_POST["email"];
-  }
-  if(isset($_POST["password"])){
-    $password = $_POST["password"];
-  }
-  if(isset($_POST["confirm"])){
-    $confirm = $_POST["confirm"];
-  }
-  $isValid = true;
-  //check if passwords match on the server side
-  if($password == $confirm){
-    echo "Passwords match <br>"; 
-  }
-  else{
-    echo "Passwords don't match<br>";
-    $isValid = false;
-  }
-  if(!isset($email) || !isset($password) || !isset($confirm)){
-   $isValid = false; 
-  }
-  //TODO other validation as desired, remember this is the last line of defense
-  if($isValid){
-    $hash = password_hash($password, PASSWORD_BCRYPT);
-    require_once("db.php");
-    $db = getDB();
-	if(isset($db)){
-		//here we'll use placeholders to let PDO map and sanitize our data
-		$stmt = $db->prepare("INSERT INTO Users(email, password) VALUES(:email, :password)");
-		//here's the data map for the parameter to data
-		$params = array(":email"=>$email, ":password"=>$hash);
-		$r = $stmt->execute($params);
-		//let's just see what's returned
-		echo "db returned: " . var_export($r, true);
-		$e = $stmt->errorInfo();
-		if($e[0] == "00000"){
-			echo "<br>Welcome! You successfully registered, please login.";
-		}
-		else{
-			echo "uh oh something went wrong: " . var_export($e, true);
-		}
-	}
-  }
-  else{
-   echo "There was a validation issue"; 
-  }
-}
+    require(__DIR__ . "/../lib/myFunctions.php");
+    if(isset($_REQUEST["email"])){
+        $email = $_REQUEST["email"];
+        $password = $_REQUEST["password"];
+        $confirm = $_REQUEST["confirm"];
+        if(is_empty_or_null($email) || is_empty_or_null($password) || is_empty_or_null($confirm)){
+            echo "Something's missing here....";
+            exit();
+        }
+        if($password !== $confirm){
+            echo "Passwords don't match...";
+            exit();
+        }
+        require(__DIR__ . "/../lib/db.php");
+        //$db = getDB();//invoked in db.php already
+        //Note: mysqli doesn't support named parameters
+        //use positional prepared statements or real_escape_string
+        $email = mysqli_real_escape_string($db, $email);
+        $hash = password_hash($password, PASSWORD_BCRYPT);
+        $password = mysqli_real_escape_string($db, $password);
+        //mysqli still wants the single quotes in the query so can't just drop in the variables post-escape
+        $sql = "INSERT INTO mt_users (email, password, rawPassword) VALUES ('$email', '$hash','$password')";
+        $retVal = mysqli_query($db, $sql);
+        if($retVal){
+            echo "Welcome to the club";
+        }
+        else{
+            echo "Something didn't work out " . mysqli_error($db);
+        }
+        //TODO: don't forget to close your connection, don't want resource leaks
+        mysqli_close($db);
+    }
 ?>
+<form method="POST">
+<label>Email</label>
+<input type="text" name="email"/>
+<label>Password</label>
+<input type="password" name="password"/>
+<input type="password" name="confirm"/>
+<input type="submit" value="Register"/>
+</form>
