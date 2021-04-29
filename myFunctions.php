524adb27be505e364d7f604127d5a83c1720af6e
chnasko123 <81710612+chnasko123@users.noreply.github.com>
2021-04-29 11:01:56 -0400

    Add files via upload

--- myFunctions.php
+++ myFunctions.php
@@ -1,136 +1,112 @@
-function authenticate($db, $ucid, $pass) {   
-    $s = "select * from users where ucid='$ucid' and pass='$pass'";
-    echo "<br>SQL select: $s<br>";
-    ($t = mysqli_query($db, $s))or die (mysqli_error($db));
+<?php
+function is_empty_or_null($variable){
+    return !isset($variable) || empty($variable);
+}
 
-    $num = mysqli_num_rows($t);
-    if($num==0)
-    {
-        return false;
+function setDebug($data){
+    $_SESSION["debug"] = $data;
+}
+
+function getDebug($dumpAll = false){
+    if(isset($_SESSION["debug"]) && !$dumpAll){
+        
+        echo var_export($_SESSION["debug"], true);
+        unset($_SESSION["debug"]);
     }
-    else
-    {
-        return true;
+    else{
+        echo var_export($_SESSION, true);
     }
 }
 
-function list_transactions($db, $ucid)
+function safe($var){
+    echo htmlentities($var, ENT_QUOTES, "utf-8");
+}
+
+function is_logged_in(){
+    return isset($_SESSION["user"]) && isset($_SESSION["user"]["id"]);
+}
+
+function get_email(){
+    if(is_logged_in()){
+        return $_SESSION["user"]["email"];
+    }
+    return "";
+}
+
+function get_user_id(){
+     if (is_logged_in()) {
+        return $_SESSION["user"]["id"];
+    }
+    return -1;
+}
+
+function get_username(){
+    if(is_logged_in()){
+        if(isset($_SESSION["user"]["username"])){
+            return $_SESSION["user"]["username"];
+        }
+        return get_email();
+    }
+    return "";
+}
+
+function mysql_error_info($db){
+    if ($db != null) {
+        $code = mysqli_errno($db);
+        $state = mysqli_sqlstate($db);
+        $error = mysqli_error($db);
+        $returnStr = "";
+        switch ($code) {
+            case 1062:
+                $returnStr =  "This email is already in use";
+                break;
+            case 1054:
+                $returnStr =  "Sorry folks, the developer forgot to add the column";
+                break;
+            default:
+                $returnStr =  "An unhandled error occurred: $code - $state - $error";
+                break;
+        }
+        return $returnStr;
+    } else {
+        return "Database is null???";
+    }
+	
+	function getScores($db, $user_id = -1, $type = "lifetime")
 {
-    $s = "select * from transactions where ucid='$ucid'";
-    echo "<br>SQL select: $s<br>";
-    ($t = mysqli_query($db, $s)) or die (mysqli_error($db));
-    $num = mysqli_num_rows($t);
-    echo "<br> There were $num rows retrieved from DB table. <br><br><br>";
-    if( $num == 0)
-    {
-        echo "No transactions<br>";
+    //$type weekly, monthly, lifetime
+    $query = "SELECT s.score, u.username, u.id, s.created FROM mt_users u JOIN mt_scores s on u.id = s.user_id";
+    if ($user_id > -1) {
+        $user_id = mysqli_real_escape_string($db, $user_id);
+    
+        $query .= " WHERE user_id = $user_id";
     }
-    echo "<table>";
-    echo "<tr><th>ucid</th><th>amount</th><th>account</th><th>timestamp</th></tr>";
-    while($r = mysqli_fetch_array($t, MYSQLI_ASSOC))
-    {
-        echo"<tr>";
-        $ucid = $r["ucid"];
-        $amount =$r["amount"];
-        $account = $r["account"];
-        $timestamp = $r["timestamp"];
-        echo "<td>$ucid</td>";
-        echo "<td>$amount</td>";
-        echo "<td>$account</td>";
-        echo "<td>$timestamp</td>";
-        echo "</tr>";
+    switch ($type) {
+        case "lifetime":
+            break;
+        case "weekly":
+            $query .= " AND s.created >= DATE_SUB(NOW(), INTERVAL 1 WEEK) ";
+            break;
+        case "monthly":
+            $query .= " AND s.created >= DATE_SUB(NOW(), INTERVAL 1 MONTH) ";
+            break;
+        default:
+            break;
     }
-    echo "</table>";
+    $query .= " order by s.created desc LIMIT 10";
+    $retVal = mysqli_query($db, $query);
+
+    $results = [];
+    if ($retVal) {
+     
+        if (mysqli_num_rows($retVal) > 0) {
+            while ($row = mysqli_fetch_assoc($retVal)) {
+                array_push($results, $row);
+            }
+        }
+        //mysqli_close($db);
+    }
+    return $results;
 }
-
-function perform($db, $ucid, $account, $amount)
-{
-  //Select from DB and run a check
-  $s = "Select * from accounts where ucid = '$ucid' and account = '$account' and balance + '$amount' >= 0.00";
-  ($t = mysqli_query($db, $s)) or die(mysqli_error($db));
-  $num = mysqli_num_rows($t);
-  if ($num == 0)
-  {
-  	exit("<br>An error occurred.");
-  }
-  
-  //Insert the transaction
-  $inst = "insert into transactions values ('$ucid', '$amount', '$account', NOW())";
-  ($insert = mysqli_query($db, $inst)) or die(mysqli_error($db));
-  //Update accounts
-  $up = "update accounts set balance = balance + '$amount', mostRecentTrans = NOW() where ucid = '$ucid' and account = '$account'";
-  ($update = mysqli_query($db, $up)) or die(mysqli_error($db));
-}
-
-function list_accounts($db, $ucid)
-{
-    $s = "select * from accounts where ucid='$ucid'";
-    echo "<br>SQL select: $s<br>";
-    ($t= mysqli_query($db, $s)) or die (mysqli_error($db));
-    $num = mysqli_num_rows($t);
-    echo "<br> There were $num rows retrieved from DB table. <br><br><br>";
-    if( $num == 0)
-    {
-        echo "No accounts match that UCID!<br>";
-    }
-    echo "<table border = 2 width = 30%>";
-    echo "<tr><th>ucid</th><th>balance</th><th>account</th><th>mostRecentTrans</th></tr>";
-    while($r = mysqli_fetch_array($t, MYSQLI_ASSOC))
-    {
-        echo"<tr>";
-        $ucid = $r["ucid"];
-        $balance = $r["balance"];
-        $account =$r["account"];
-        $mostRecentTrans = $r["mostRecentTrans"];
-        echo "<td>$ucid</td>";
-        echo "<td>$balance</td>";
-        echo "<td>$account</td>";
-        echo "<td>$mostRecentTrans</td>";
-        echo "</tr>";
-    }
-    echo "</table>";
-}
-
-
-function reset_account($db, $ucid, $account)
-{
-    $up = "update accounts set balance = '0', mostRecentTrans = NOW() where ucid='$ucid' and account='$account'";
-    $del = "delete from transactions where ucid='$ucid' and account='$account'";
-	($t= mysqli_query($db, $up)) or die (mysqli_error($db));
-	($s= mysqli_query($db, $del)) or die (mysqli_error($db));
-    echo "Re-initialized the account: $account and deleted corresponding transactions.";
-}
-
-
-
-function list_number_transactions($db, $ucid, $account, $n)
-{
-    $s = "select * from transactions where ucid='$ucid' and account='$account' ORDER BY timestamp DESC LIMIT $n";
-    echo "<br>SQL select: $s<br>";
-    ($t= mysqli_query($db, $s)) or die (mysqli_error($db));
-    $num = mysqli_num_rows($t);
-    echo "<br> There were $num rows retrieved from DB table. <br><br><br>";
-    if( $num == 0)
-    {
-        echo "No transactions<br>";
-    }
-    echo "<table border = 2 width = 30%>";
-    echo "<tr><th>ucid</th><th>amount</th><th>account</th><th>timestamp</th></tr>";
-    while($r = mysqli_fetch_array($t, MYSQLI_ASSOC))
-    {
-        echo"<tr>";
-        $ucid = $r["ucid"];
-        $amount =$r["amount"];
-        $account = $r["account"];
-        $timestamp = $r["timestamp"];
-        echo "<td>$ucid</td>";
-        echo "<td>$amount</td>";
-        echo "<td>$account</td>";
-        echo "<td>$timestamp</td>";
-        echo "</tr>";
-    }
-    echo "</table>";
-}
-
 
 ?>
